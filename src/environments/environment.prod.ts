0a6602a84497da830a8c1b285596738c66760715
billydragon17 <130969160+billydragon17@users.noreply.github.com>
2024-03-24 20:48:14 +0100

    Setup configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiHost: 'https://readonlydemo.vendure.io',
+    apiHost: 'https://localhost:3000',
     apiPort: 443,
     shopApiPath: 'shop-api',
     baseHref: '/',
